ed6e668f6d0a655dcff84b57af94a1d410ae59d6
thiagohwalter <thiagohwalter@gmail.com>
2017-05-05 09:28:06 -0300

    Upload dos arquivos iniciais da aplicação

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require 'logica.php'; ?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <title>Gerador</title>
+</head>
+<body>
+
+
+    <h2> <?= "Eai $tratamento $nome, $qualidade $lugar"; ?></h2>
+
+
+</body>
+</html>
---
+++ logica.php
@@ -0,0 +1,9 @@
+<?php
+    $tratamentos = [' Dom ',' Grão ',' Major '];
+    $qualidades  = [' Paladino ',' Estupefato ',' Auror '];
+    $lugares      = [' Das terras médias ',' Do Beco Diagonal ',' de Hogwarts '];
+    $nome       = "Christian, ";
+
+    $tratamento = $tratamentos[ rand(0,2)];
+    $qualidade  = $qualidades[ rand(0,2)];
+    $lugar      = $lugares[ rand(0,2)];
